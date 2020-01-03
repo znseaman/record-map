e7785faca269019094d553ba16e203664ba8aff9
Zach Seaman <znseaman@gmail.com>
2020-01-03 09:28:01 -0800

    improv(dx): remove console.log, window tidy functions

--- client/src/components/Draw.js
+++ client/src/components/Draw.js
@@ -54,54 +54,23 @@ const Draw = props => {
     control.current.draw.set(layer);
   };
 
-  const assignRef = drawControl => {
-    control.current = drawControl;
-    if (process.env.NODE_ENV == "development") {
-      window.drawControl = drawControl;
-    }
-  };
+  const assignRef = drawControl => control.current = drawControl;
 
-  const onDrawCreate = ({ features }) => {
-    console.log(`onDrawCreate`, features);
-    add(features);
-  };
+  const onDrawCreate = ({ features }) => add(features);
 
-  const onDrawDelete = event => {
-    console.log(`onDrawDelete`, event);
-    const { type, features } = event;
-    if (type === "draw.delete") {
-      remove(features);
-    }
-  };
+  const onDrawDelete = ({ features }) => remove(features);
 
-  const onDrawCombine = async event => {
-    console.log(`onDrawCombine`, event);
-    const { type, createdFeatures, deletedFeatures } = event;
-    if (type === "draw.combine") {
-      await combine({ deletedFeatures, createdFeatures });
-    }
-  };
+  const onDrawCombine = ({ createdFeatures, deletedFeatures }) => combine({ deletedFeatures, createdFeatures });
 
-  const onDrawUncombine = async event => {
-    console.log(`onDrawUncombine`, event);
-    const { type, createdFeatures, deletedFeatures } = event;
-    if (type === "draw.uncombine") {
-      await uncombine({ deletedFeatures, createdFeatures });
-    }
-  };
+  const onDrawUncombine = ({ createdFeatures, deletedFeatures }) => uncombine({ deletedFeatures, createdFeatures });
 
-  const onDrawUpdate = event => {
-    console.log(`onDrawUpdate`, event);
-    const { type, features, action } = event;
-
+  const onDrawUpdate = ({ type, features, action }) => {
     if (type === "draw.update" && (action == "move" || action == "change_coordinates")) {
       selectedFeatures.current = features;
     }
   };
 
   const onDrawSelectionChange = event => {
-    console.log(`onDrawSelectionChange`, event);
-
     // Zero features means the user has clicked away from adding a feature ("direct_select")
     if (event.features.length == 0 && selectedFeatures.current.length > 0) {
       save(selectedFeatures.current);
@@ -110,21 +79,7 @@ const Draw = props => {
     }
   };
 
-  const onDrawActionable = event => {
-    // @TODO: instead of saving all of them, determine which ones have changed and save those instead
-    // console.log(`onDrawActionable`, event)
-  };
-
-  const onDrawModeChange = event => {
-    if (event.mode == "direct_select") {
-      //   @TODO: start an edit mode
-    }
-    console.log(`onDrawModeChange`, event);
-  };
-
-  const onDrawRender = event => {
-    // console.log(`onDrawRender`, event);
-  };
+  const noOp = () => { };
 
   return (
     <DrawControl
@@ -135,9 +90,9 @@ const Draw = props => {
       onDrawUncombine={onDrawUncombine}
       onDrawUpdate={onDrawUpdate}
       onDrawSelectionChange={onDrawSelectionChange}
-      onDrawActionable={onDrawActionable}
-      onDrawModeChange={onDrawModeChange}
-      onDrawRender={onDrawRender}
+      onDrawActionable={noOp}
+      onDrawModeChange={noOp}
+      onDrawRender={noOp}
     ></DrawControl>
   );
 };
